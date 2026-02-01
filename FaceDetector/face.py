0debcd1cb5b5e8f2bfa554cddd9f35d3f3e3c954
Sonic <sonicsmritshrestha@gmail.com>
2026-02-01 22:48:47 +0545

    Update: Eye Tracking added too

--- FaceDetector/face.py
+++ FaceDetector/face.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 
 face_cascade = cv.CascadeClassifier('FaceDetector/haarcascade_frontalface_default.xml')
-
+eye_cascade = cv.CascadeClassifier('FaceDetector/haarcascade_eye_tree_eyeglasses.xml')
 
 Video = cv.VideoCapture(0)
 
@@ -16,6 +16,7 @@ while True:
     gray = cv.cvtColor(vid, cv.COLOR_BGR2GRAY)
 
     face = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10)
+    eye = eye_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
     faces = len(face)
     
     text = f"Number of Faces {faces}"
@@ -24,7 +25,12 @@ while True:
         cv.rectangle(vid, (x, y), (x+w, y+h), (255, 0, 0), 2)
         
         cv.putText(vid, text,(0,30), cv.FONT_HERSHEY_COMPLEX,1,(0, 255, 0), 3)
+    
+    for (x,y,w,h) in eye:
+        cv.circle(vid, (x+w//2,y+h//2), 30, (0,0,0),3)
         
+    
+
 
 
 
